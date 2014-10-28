1e25cb2060015a3426bbb8b3026e0e3a130d8402
Jay Wengrow <jaywngrw@gmail.com>
2014-10-28 14:00:13 -0500

    creation of order

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,7 +1,9 @@
 class CartedProductsController < ApplicationController
 
   def create
-    
+    current_user.orders.create(:status => "cart") unless current_user.orders.where(:status => "cart").any?
+    redirect_to root_path
+
   end
 
 end
