3a8ec98820c1b0eb41e98447436526891096a8ca
Jay Wengrow <jaywngrw@gmail.com>
2014-10-28 16:21:08 -0500

    adding products to shopping cart

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,9 +1,14 @@
 class CartedProductsController < ApplicationController
 
   def create
-    current_user.orders.create(:status => "cart") unless current_user.orders.where(:status => "cart").any?
+    if current_user.orders.where(:status => "cart").empty?
+      @order = current_user.orders.create(:status => "cart")
+    else
+      @order = current_user.orders.find_by(:status => "cart")
+    end
+    @carted_product = @order.carted_products.create(params[:carted_product])
+    flash[:success] = "Product added to shopping cart."
     redirect_to root_path
-
   end
 
 end
